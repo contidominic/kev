fd65f670fbbe14a7e24cd508464ac305e493a36b
contidominic <74687438+contidominic@users.noreply.github.com>
2021-01-21 16:58:25 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,9 @@ export default function Contact() {
   const [state, setState] = React.useState({})
 
   const handleChange = (e) => {
+   alert(e.target.name +" " + e.target.value)
     setState({ ...state, [e.target.name]: e.target.value })
+  
   }
 
   const handleSubmit = (e) => {
